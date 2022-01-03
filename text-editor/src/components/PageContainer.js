1d9770c0b2f48cea02cda59a31eab6b5df7e2ae1
Ravisha Sharma <ravisha7feb@gmail.com>
2022-01-03 13:32:46 +0530

    modify app.js and add editorstate empty constructor

--- text-editor/src/components/PageContainer.js
+++ text-editor/src/components/PageContainer.js
@@ -1,15 +1,23 @@
 import React from "react";
 import { Editor, EditorState, RichUtils } from "draft-js";
 
-//render editor component within the PageContainer class
 class PageContainer extends React.Component {
+    constructor(props) {
+        super(props) //used to call the constructor of its parent class
+        this.state = {
+			editorState: EditorState.createEmpty() //sets its content to empty
+		};
+    }
+    //pass this as prop in editor component
     render () {
         return (
-            <div>
-                <Editor/>
+            <div> 
+                <p>Work in progress</p>
+                <Editor editorState={this.state.editorState} /> 
             </div>
             
-        )
-        
+        );
+
     }
 }
+export default PageContainer;
