8138df32c5c1c2c6cc9acd74ea883149a4afb57f
Ravisha Sharma <ravisha7feb@gmail.com>
2022-01-03 14:08:56 +0530

    add comments and fix indentations

--- text-editor/src/components/PageContainer.js
+++ text-editor/src/components/PageContainer.js
@@ -1,33 +1,33 @@
-import React from 'react'
-import { Editor, EditorState } from 'draft-js';
+import React from "react";
+import { Editor, EditorState, RichUtils } from "draft-js";
 
 class PageContainer extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      editorState: EditorState.createEmpty(),
-    }; 
-  }
+    constructor(props) {
+        super(props) //used to call the constructor of its parent class
+        this.state = {
+			editorState: EditorState.createEmpty() //sets its content to empty
+		};
+    }
+    //create onChange function and pass this also as a prop in editor
+    onChange = editorState => {
+		this.setState({
+			editorState
+		});
+	};
+    //pass these as props in editor component
+    render () {
+        return (
+            <div className="editorContainer"> 
+                <div className="editors">
+                <Editor editorState={this.state.editorState} 
+                        onChange = {this.onChange} 
+                        placeholder="Click here and start typing your text.."
+                        /> 
+                </div>
+            </div>
+            
+        );
 
-  onChange = (editorState) => {
-    this.setState({
-      editorState
-    })
-  }
-
-  render() {
-    return (
-      <div className="editorContainer" >
-       <div className="editors">
-        <Editor
-          editorState={this.state.editorState}
-          onChange={this.onChange}
-          placeholder="Write your text here.."
-        />
-        </div>
-      </div>
-    )
-  }
+    }
 }
-
-export default PageContainer
+export default PageContainer;
